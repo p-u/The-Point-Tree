aee61e440434a17a06bf6dbe9f2c2101a28cf69d
randim82 <otherrtthing@gmail.com>
2023-12-18 11:13:06 +0800

    V1.0: REBIRTH!!
    4 NEW BASIC POINT UPGRADES
    NEW RESET LAYER: REBIRTH!
    1 NEW REBIRTH UPGRADE
    MORE STUFF TO CHANGELOG
    BU7,BU8 BUFF
    BU3, BU5 NERF

--- js/layers.js
+++ js/layers.js
@@ -29,7 +29,7 @@ addLayer("basic", {
             description: "Point Fragments boost basic points.",
             cost: new Decimal(5),
             effect() {
-                return player.points.add(1).pow(0.185)
+                return player.points.add(1).pow(0.16)
             },
             effectDisplay() { return format(upgradeEffect(this.layer, this.id))+"x" }, // Add formatting to the effect
             unlocked() { return hasUpgrade("basic", 12) },
@@ -45,7 +45,7 @@ addLayer("basic", {
             description: "Basic Points boosts itself.",
             cost: new Decimal(25),
             effect() {
-                return player[this.layer].points.add(1).pow(0.2)
+                return player[this.layer].points.add(1).pow(0.175)
             },
             effectDisplay() { return format(upgradeEffect(this.layer, this.id))+"x" }, // Add formatting to the effect
             unlocked() { return hasUpgrade("basic", 14) },
@@ -58,19 +58,47 @@ addLayer("basic", {
         },
         22: {
             title: "Upgrade 7",
-            description: "Basic Points are multiplied by 1.22",
+            description: "Basic Points are multiplied by 1.39",
             cost: new Decimal(250),
             unlocked() { return hasUpgrade("basic", 21) },
         },
         23: {
             title: "Upgrade 8",
-            description: "Point Fragments boosts itself slightly",
+            description: "Point Fragments boosts itself",
             cost: new Decimal(500),
             effect() {
-                return player.points.add(1).pow(0.125)
+                return player.points.add(1).pow(0.1625)
             },
             effectDisplay() { return format(upgradeEffect(this.layer, this.id))+"x" }, // Add formatting to the effect
             unlocked() { return hasUpgrade("basic", 22) },
+        },
+        24: {
+            title: "Upgrade 9",
+            description: "Point fragments are TRIPLED!!",
+            cost: new Decimal(1500),
+            unlocked() { return hasUpgrade("basic", 23) },
+        },
+        25: {
+            title: "Upgrade 10",
+            description: "Point fragments boost itself, again, but less",
+            cost: new Decimal(5500),
+            effect() {
+                return player.points.add(500000).pow(0.055)
+            },
+            effectDisplay() { return format(upgradeEffect(this.layer, this.id))+"x" }, // Add formatting to the effect
+            unlocked() { return hasUpgrade("basic", 24) },
+        },
+        31: {
+            title: "Upgrade 11",
+            description: "Point fragments are multiplied by 2.5",
+            cost: new Decimal(20000),
+            unlocked() { return hasUpgrade("basic", 25) },
+        },
+        32: {
+            title: "Upgrade 12",
+            description: "The final upgrade before the next reset layer: X5 POINT FRAGMENTS!!",
+            cost: new Decimal(60000),
+            unlocked() { return hasUpgrade("basic", 31) },
         },
     },
     color: "#add8e6",
@@ -85,6 +113,7 @@ addLayer("basic", {
         if (hasUpgrade('basic', 13)) mult = mult.times(upgradeEffect('basic', 13))
         if (hasUpgrade('basic', 15)) mult = mult.times(upgradeEffect('basic', 15))
         if (hasUpgrade('basic', 14)) mult = mult.times(1.35)
+        if (hasUpgrade('basic', 22)) mult = mult.times(1.39)
         return mult
     },
     gainExp() { // Calculate the exponent on main currency from bonuses
@@ -93,6 +122,47 @@ addLayer("basic", {
     row: 0, // Row the layer is in on the tree (0 is the first row)
     hotkeys: [
         {key: "b", description: "B: Reset for basic points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+    ],
+    layerShown(){return true}
+}),
+addLayer("rebirth", {
+    name: "Rebirth Points", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "R", // This appears on the layer's node. Default is the id with the first letter capitalized
+    position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    startData() { return {
+        unlocked: false,
+		points: new Decimal(0),
+    }},
+    layerShown(){
+        let visible = false
+        If (hasUpgrade('basic', 32)); visible = true
+       return visible
+     },
+    upgrades: {
+        11: {
+            title: "Welcome to rebirth. Here's a x4 point fragments for you.",
+            description: "x4 Point fragments..",
+            cost: new Decimal(1),
+
+        },
+    },
+    color: "#00008b",
+    requires: new Decimal(50000000), // Can be a function that takes requirement increases into account
+    resource: "Rebirth Points", // Name of currency
+    baseResource: "Point Fragments", // Name of resource prestige is based on
+    baseAmount() {return player.points}, // Get the current amount of baseResource
+    type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    exponent: 0.12, // Prestige currency exponent
+    gainMult() { // Prestige multiplier
+        let mult = new Decimal(1)
+        return mult
+    },
+    gainExp() { // Calculate the exponent on main currency from bonuses
+        return new Decimal(1)
+    },
+    row: 1, // Row the layer is in on the tree (0 is the first row)
+    hotkeys: [
+        {key: "r", description: "R: Reset for Rebirth points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
     layerShown(){return true}
 })
--- js/mod.js
+++ js/mod.js
@@ -13,22 +13,31 @@ let modInfo = {
 
 // Set your version in num and name
 let VERSION = {
-	num: "0.3",
-	name: "3 more upgrades, up to 8 total!",
+	num: "1.0",
+	name: "Rebirth + 5 New upgrades (13 Total)",
 }
 
 let changelog = `<h1>Changelog:</h1><br>
+<h3>v1.0</h3><br>
+- Decrease BU3 and BU5 Scaling.<br>
+- Increase BU7 and BU8 Scaling.<br>
+- Added 4 new point upgrades and 1 new rebirth upgrade!<br>
+- ADDED REBIRTH RESET LAYER.<br>
+- Current Endgame: RU1 (Or 200M Point Fragments, 2M Basic Points).<br>
 <h3>v0.3</h3><br>
 - Decrease BU2's scaling by a lot.<br>
 - Added 3 more upgrades, again.<br>
 - Changed ID.<br>
+- Current Endgame: 1.5K Basic Points (or 200K Point Fragments).<br>
     <h3>v0.2</h3><br>
 		- Decrease BU2's cost.<br>
 		- Added 3 more upgrades.<br>
 		- Made changelog work.<br>
+		- Current Endgame: 20 points a sec.<br>
 	<h3>v0.1</h3><br>
 		- One upgrade.<br>
-		- Added Basic Points and Point Fragments.`
+		- Added Basic Points and Point Fragments.
+		- Current Endgame: 6 points a sec.<br>`
 
 let winText = `Congratulations! You have reached the end and beaten this game, but for now...`
 
@@ -53,9 +62,14 @@ function getPointGen() {
 	let gain = new Decimal(1)
 	if (hasUpgrade('basic', 12)) gain = gain.times(upgradeEffect('basic', 12))
 	if (hasUpgrade('basic', 23)) gain = gain.times(upgradeEffect('basic', 23))
+	if (hasUpgrade('basic', 25)) gain = gain.times(upgradeEffect('basic', 25))
 	if (hasUpgrade('basic', 11)) gain = gain.times(2)
 	if (hasUpgrade('basic', 21)) gain = gain.times(2)
+	if (hasUpgrade('basic', 24)) gain = gain.times(3)
+	if (hasUpgrade('basic', 31)) gain = gain.times(2.5)
+	if (hasUpgrade('basic', 32)) gain = gain.times(5)
 	if (hasUpgrade('basic', 14)) gain = gain.times(1.35)
+	if (hasUpgrade('rebirth', 11)) gain = gain.times(4)
 	return gain
 }
 
