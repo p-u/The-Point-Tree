576014438d746dabc5c47476ad6a4fc4f912c448
randim82 <otherrtthing@gmail.com>
2023-12-19 15:05:40 +0800

    v7.0

--- js/mod.js
+++ js/mod.js
@@ -7,7 +7,7 @@ let modInfo = {
 
 	discordName: "SR46A",
 	discordLink: "",
-	initialStartPoints: new Decimal("e1500"), // Used for hard resets and new players
+	initialStartPoints: new Decimal("0"), // Used for hard resets and new players
 	offlineLimit: 10,  // In hours
 }
 
