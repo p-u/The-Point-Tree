26490b60dcafe3fcde569fb9482018943e3ef25a
randim82 <otherrtthing@gmail.com>
2024-01-02 15:39:37 +0800

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -1263,7 +1263,7 @@ addLayer("sac", {
     position: 1, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
         unlocked: false,
-		points: new Decimal(10),
+		points: new Decimal(0),
     }},
     layerShown(){
         let visible = false
