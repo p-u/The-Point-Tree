b1fa8e5f698ca30fa05ecbe10f1329b58a104fe1
randim82 <otherrtthing@gmail.com>
2025-10-01 18:37:04 +0800

    MAJOR HOTFIX

--- js/energy.js
+++ js/energy.js
@@ -62,7 +62,7 @@ addLayer("e", {
         // Stage 3, track which main features you want to keep - milestones
         let keep = [];
         if (hasMilestone('sac', 20)) keep.push("milestones");
-        if (hasMilestone("era", 3)) keep.push("upgrades");
+        if ((hasMilestone("era", 3)) && (!(hasAchievement("a", 266)))) keep.push("upgrades");
         if (hasMilestone("era", 3)) keep.push("milestones");
     
         // Stage 4, do the actual data reset
