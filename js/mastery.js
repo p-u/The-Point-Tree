f383124a1e28457611f1f664036d3b15f5e7306f
randim82 <otherrtthing@gmail.com>
2025-10-18 19:52:09 +0800

    Update mastery.js

--- js/mastery.js
+++ js/mastery.js
@@ -829,11 +829,11 @@ addLayer("m", {
             if (player.m.rngpower.gte(0.625)) {
                 player.m.rngpower = new Decimal(0.62)
             }
-            if ((player.m.rngpower.gte(0.24) && (!(hasUpgrade("e", 223))))) {
-                player.m.rngpower = new Decimal(0.208)
+            if ((player.m.rngpower.gte(0.5) && (!(hasUpgrade("e", 223))))) {
+                player.m.rngpower = new Decimal(0.421)
             }
-            if ((player.m.rngpower.gte(0.13) && (!(hasUpgrade("e", 212))))) {
-                player.m.rngpower = new Decimal(0.106)
+            if ((player.m.rngpower.gte(0.27) && (!(hasUpgrade("e", 212))))) {
+                player.m.rngpower = new Decimal(0.218)
             }
             player.m.totalUps = new Decimal((player.basic.upgrades + player.basic.milestones + player.rebirth.milestones + player.rebirth.upgrades + player.prestige.milestones + player.prestige.upgrades + player.mega.upgrades + player.mega.milestones + player.e.milestones + player.e.upgrades + player.w.milestones + player.w.upgrades + player.sac.milestones + player.era.upgrades + player.era.milestones + player.era.points + player.s.milestones + player.s.upgrades).length)
             let differ = player.m.totalUps.sub(player.m.lastUps)
