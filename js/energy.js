4c0859c7a7850378b6eb284d63e1a7f2b1ef9c97
randim82 <otherrtthing@gmail.com>
2026-05-21 22:59:30 +0800

    Update energy.js

--- js/energy.js
+++ js/energy.js
@@ -221,7 +221,7 @@ addLayer("e", {
         }
         if (mult.gte(2)) mult = mult.div(2)
         if (mult.gte(2)) mult = mult.div(2)
-        if (mult.gte(1.7)) mult = mult.div(1.7)
+        if (mult.gte(1.9)) mult = mult.div(1.9)
         return mult
     },
     gainExp() { // Calculate the exponent on main currency from bonuses
