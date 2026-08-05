d800fec3a848890dca46c632581dfa47bce08045
randim82 <otherrtthing@gmail.com>
2026-08-05 16:06:41 +0800

    another one

--- js/layer.js
+++ js/layer.js
@@ -249,7 +249,7 @@ addLayer("p", {
                 if (hasMilestone("p",21) && (player.aura.totalRolls>20000)) mult = mult.mul(2)
                 if (hasMilestone("p",21) && (player.aura.totalRolls>75000)) mult = mult.mul(2)
                 if (hasMilestone("p",21) && (player.points.gte("e15e6"))) mult = mult.mul(2)
-                return "Gain "+ notationChooser(player.points.add(1).log10().add(1).log(1.1).mul(player.p.points.add(1).log(1.1)).mul(player.p.holdCombo+1).mul(mult)) +" Energy! (HOLD THE BUTTON, NOT CLICK IT!)";
+                return "Gain "+ notationChooser(player.points.add(1).log10().add(1).log(1.1).mul(player.p.points.add(1).log(1.1)).mul(player.p.holdCombo+1).mul(mult)) +" Energy! (Clicking gives 20% of the Energy as Holding)";
             },
             canClick() { return true; },
             canHold() { return true; },
